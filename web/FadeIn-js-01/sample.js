008d4fbef86508e6f8548e612acea472c420cf3d
田村 一希 <kazukitamura0905@gmail.com>
2020-06-14 03:32:35 +0900

    js修正

--- web/FadeIn-js-01/sample.js
+++ web/FadeIn-js-01/sample.js
@@ -1,22 +1,21 @@
-$(function(){
-    function animation(){
-      //eachで順番にliの要素へ働きかけている
-      $('.fadeInUp').each(function(){
-        //ターゲットの位置を取得
-        var target = $(this).offset().top;
-        //スクロール量を取得
-        var scroll = $(window).scrollTop();
-        //ウィンドウの高さを取得。繰り返し処理内で常に一定の値
-        var windowHeight = $(window).height();
-        //ターゲットまでスクロールするとフェードインする
-        if (scroll > target - windowHeight){
-          $(this).css('opacity','1');
-          $(this).css('transform','translateY(0)');
-        }
-      });
+function animation() {
+  //eachで順番にfadeInUpクラス（li）の要素へ働きかけている
+  $('.fadeInUp').each(function () {
+    //ターゲットの位置を取得
+    var target = $(this).offset().top;
+    //スクロール量を取得
+    var scroll = $(window).scrollTop();
+    //ウィンドウの高さを取得。繰り返し処理内で常に一定の値
+    var windowHeight = $(window).height();
+    //ターゲットまでスクロールするとフェードインする
+    if (scroll > target - windowHeight) {
+      $(this).css('opacity', '1');
+      $(this).css('transform', 'translateY(0)');
     }
-    animation();
-    $(window).scroll(function (){
-      animation();
-    });
   });
+}
+
+//windowオブジェクトを指定
+$(window).scroll(
+  animation()
+);
