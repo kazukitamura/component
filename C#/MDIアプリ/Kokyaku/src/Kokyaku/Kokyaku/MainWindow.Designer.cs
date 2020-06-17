2d4ab97a1febfb231c39efeb8483f9a4c56fefa2
田村 一希 <kazukitamura0905@gmail.com>
2020-06-17 15:47:23 +0900

    Multiple Document Interfaceのタタキ台

---
+++ C#/MDIアプリ/Kokyaku/src/Kokyaku/Kokyaku/MainWindow.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Kokyaku
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 433);
+            this.IsMdiContainer = true;
+            this.Name = "MainWindow";
+            this.Text = "顧客管理";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
