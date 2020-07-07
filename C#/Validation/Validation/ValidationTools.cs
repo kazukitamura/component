1bc7ebd6cfdb5da67e562b339c4fca66a4598642
kazukitamura <kazukitamura0905@gmail.com>
2020-07-07 17:09:43 +0900

    ValidationTools追加

--- C#/Validation/Validation/ValidationTools.cs
+++ C#/Validation/Validation/ValidationTools.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,5 +21,43 @@ namespace Validation
             return true;
 
         }
+
+        public static bool RequiredCheck(string s)
+        {
+            if (s.Length == 0)
+            {
+                Console.WriteLine("必須Check:               [NG]");
+                return false;
+
+            }
+            Console.WriteLine("RequiredCheck:               [OK]");
+            return true;
+        }
+
+        public static bool DigitsCheck(string s, int digits)
+        {
+            if (s.Length > digits)
+            {
+                Console.WriteLine("桁数Check:               [NG]");
+                return false;
+
+            }
+            Console.WriteLine("桁数Check:               [OK]");
+            return true;
+
+        }
+
+        public static bool NumberCheck(string s)
+        {
+            int o;
+            if (!int.TryParse(s, out o))
+            {
+                Console.WriteLine("数値Check:               [NG]");
+                return false;
+            }
+            Console.WriteLine("数値Check:               [OK]");
+            return true;
+        }
+
     }
 }
