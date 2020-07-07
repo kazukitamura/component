abafbc9f536ccaddd9195ae98af5e5355dbfbafe
kazukitamura <kazukitamura0905@gmail.com>
2020-07-07 17:27:22 +0900

    Validatiorクラス作成

---
+++ C#/Validation/Validation/Validatior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    class Validatior
+    {
+        private readonly List<IValidation> validationList = new List<IValidation>();
+
+        public List<IValidation> ValidationList
+        {
+            get
+            {
+                return validationList;
+            }
+        }
+
+        public void Validate()
+        {
+            foreach(IValidation item in validationList)
+            {
+                item.Validate();
+            }
+        }
+    }
+}
