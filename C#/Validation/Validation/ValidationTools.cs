20f20bea554747be1524e35b1444369e24987acd
kazukitamura <kazukitamura0905@gmail.com>
2020-07-07 11:44:45 +0900

    val進めた

---
+++ C#/Validation/Validation/ValidationTools.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    static class ValidationTools
+    {
+        public static bool NullCheck(string s)
+        {
+            if (s == null)
+            {
+                Console.WriteLine("NullCheck:               [NG]");
+                return false;
+
+            }
+            Console.WriteLine("NullCheck:               [OK]");
+            return true;
+
+        }
+    }
+}
